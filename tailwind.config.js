3850be8b9573369eb6afde6cb96df0e42a6f1281
Ani Jekova <angelina.jekova@rubberduck.xyz>
2021-06-29 17:54:12 +0300

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+    },
+  },
+  plugins: [],
+}
